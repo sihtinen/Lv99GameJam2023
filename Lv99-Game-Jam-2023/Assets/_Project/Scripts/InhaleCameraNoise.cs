577b79f6c6cb76d415dafb491a3cdb111f8ee057
Sami Lahtinen <sihtinen@gmail.com>
2023-04-18 19:16:30 +0300

    added camera noise shake effect when inhaling

---
+++ Lv99-Game-Jam-2023/Assets/_Project/Scripts/InhaleCameraNoise.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Cinemachine;
+
+public class InhaleCameraNoise : SingletonBehaviour<InhaleCameraNoise>
+{
+    [SerializeField] private float m_maxGain = 1.0f;
+
+    private CinemachineVirtualCamera m_vcam = null;
+    private CinemachineBasicMultiChannelPerlin m_noise = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        TryGetComponent(out m_vcam);
+        m_noise = m_vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        m_noise.m_AmplitudeGain = 0f;
+    }
+
+    private void Update()
+    {
+        float _targetGain = 0f;
+
+        var _inhaleComponent = PlayerInhaleComponent.Instance;
+
+        if (_inhaleComponent != null && _inhaleComponent.IsInhaling)
+            _targetGain = m_maxGain;
+
+        m_noise.m_AmplitudeGain = Mathf.MoveTowards(m_noise.m_AmplitudeGain, _targetGain, Time.deltaTime);
+    }
+}
