091234adb80cd4a2bf3396b1b7f224133aa6f981
Sami Lahtinen <sihtinen@gmail.com>
2023-04-20 10:03:38 +0300

    added ability icons to UI elements, renamed decal materials, added all puzzle scenes to main menu level select

--- Lv99-Game-Jam-2023/Assets/_Project/Scripts/AvailableAbilityElement.cs
+++ Lv99-Game-Jam-2023/Assets/_Project/Scripts/AvailableAbilityElement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
 
 using TMPro;
 
@@ -13,7 +14,13 @@ public class AvailableAbilityElement : MonoBehaviour
     [SerializeField] private AnimationCurve m_spawnAnimationScaleCurve = new AnimationCurve();
 
     [Header("Object References")]
-    [SerializeField] private TMP_Text m_nameText = null;
+    [SerializeField] private TMP_Text m_inputText = null;
+    [SerializeField] private Image m_iconImage = null;
+    [Space]
+    [SerializeField] private Sprite m_jumpIconSprite = null;
+    [SerializeField] private Sprite m_meleeIconSprite = null;
+    [SerializeField] private Sprite m_inhaleIconSprite = null;
+    [SerializeField] private Sprite m_timestopIconSprite = null;
 
     [NonSerialized] public AbilityTypes AbilityType;
 
@@ -28,7 +35,28 @@ public class AvailableAbilityElement : MonoBehaviour
     public void BindToAbilityAndActivate(AbilityTypes abilityType)
     {
         AbilityType = abilityType;
-        m_nameText.SetText(abilityType.ToString());
+
+        switch (AbilityType)
+        {
+            default:
+            case AbilityTypes.Jump:
+                m_iconImage.overrideSprite = m_jumpIconSprite;
+                m_inputText.SetText("Space");
+                break;
+            case AbilityTypes.Melee:
+                m_iconImage.overrideSprite = m_meleeIconSprite;
+                m_inputText.SetText("X");
+                break;
+            case AbilityTypes.Inhale:
+                m_iconImage.overrideSprite = m_inhaleIconSprite;
+                m_inputText.SetText("C");
+                break;
+            case AbilityTypes.Timestop:
+                m_iconImage.overrideSprite = m_timestopIconSprite;
+                m_inputText.SetText("V");
+                break;
+        }
+
         gameObject.SetActiveOptimized(true);
         StartCoroutine(coroutine_onSpawn());
     }
--- Lv99-Game-Jam-2023/Assets/_Project/Scripts/MeditationAbilitySelectionUIElement.cs
+++ Lv99-Game-Jam-2023/Assets/_Project/Scripts/MeditationAbilitySelectionUIElement.cs
@@ -3,13 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
 
 using TMPro;
 
 public class MeditationAbilitySelectionUIElement : MonoBehaviour
 {
     [Header("Object References")]
-    [SerializeField] private TMP_Text m_labelText = null;
+    [SerializeField] private Image m_iconImage = null;
+    [SerializeField] private TMP_Text m_ablityNameText = null;
+    [SerializeField] private TMP_Text m_inputText = null;
+    [Space]
+    [SerializeField] private Sprite m_jumpIconSprite = null;
+    [SerializeField] private Sprite m_meleeIconSprite = null;
+    [SerializeField] private Sprite m_suctionIconSprite = null;
+    [SerializeField] private Sprite m_timestopIconSprite = null;
 
     [NonSerialized] public AbilityTypes AbilityType;
     [NonSerialized] public RectTransform RectTransform = null;
@@ -22,7 +30,29 @@ public class MeditationAbilitySelectionUIElement : MonoBehaviour
     public void BindToAbility(AbilityTypes abilityType)
     {
         AbilityType = abilityType;
-        m_labelText.SetText(AbilityType.ToString());
+        m_ablityNameText.SetText(AbilityType.ToString());
+
+        switch (AbilityType)
+        {
+            default:
+            case AbilityTypes.Jump:
+                m_iconImage.overrideSprite = m_jumpIconSprite;
+                m_inputText.SetText("Press \"<b>Space</b>\"");
+                break;
+            case AbilityTypes.Melee:
+                m_iconImage.overrideSprite = m_meleeIconSprite;
+                m_inputText.SetText("Press \"<b>X</b>\"");
+                break;
+            case AbilityTypes.Inhale:
+                m_iconImage.overrideSprite = m_suctionIconSprite;
+                m_inputText.SetText("Press \"<b>C</b>\"");
+                break;
+            case AbilityTypes.Timestop:
+                m_iconImage.overrideSprite = m_timestopIconSprite;
+                m_inputText.SetText("Press \"<b>V</b>\"");
+                break;
+        }
+
         transform.SetAsLastSibling();
         gameObject.SetActive(true);
     }
